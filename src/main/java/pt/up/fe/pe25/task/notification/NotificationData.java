f75764285df3175021d9942f8d0c2e8d65b689f3
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2023-02-23 22:58:46 +0000

    Embedded notification data

--- src/main/java/pt/up/fe/pe25/task/notification/NotificationData.java
+++ src/main/java/pt/up/fe/pe25/task/notification/NotificationData.java
@@ -1,4 +1,19 @@
 package pt.up.fe.pe25.task.notification;
 
-public record NotificationData() {
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class NotificationData {
+
+    @Column(name = "sender_phone")
+    private String senderPhone;
+
+    @Column(name = "receiver_phone")
+    private String receiverPhone;
+
+    @Column(name = "message")
+    private String message;
+
+    // TODO: Add more fields ... Some will be more specific to each service and will probably be added in another place
 }
--- src/main/java/pt/up/fe/pe25/task/notification/Notifier.java
+++ src/main/java/pt/up/fe/pe25/task/notification/Notifier.java
@@ -1,8 +1,10 @@
 package pt.up.fe.pe25.task.notification;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -17,7 +19,12 @@ public class Notifier extends PanacheEntity {
     private List<String> notificationServices;
 
 
+    @Embedded
+    private NotificationData notificationData;
 
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 
 }
