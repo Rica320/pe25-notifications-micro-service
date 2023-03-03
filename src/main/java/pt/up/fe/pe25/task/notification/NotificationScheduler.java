0682e849bc816ed0b65b0d0bcd9d9dd53eca82ed
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2023-03-03 15:45:43 +0000

    Notification schedule

---
+++ src/main/java/pt/up/fe/pe25/task/notification/NotificationScheduler.java
@@ -0,0 +1,47 @@
+package pt.up.fe.pe25.task.notification;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.time.ZoneId;
+import java.util.Date;
+
+@ApplicationScoped
+public class NotificationScheduler {
+
+    public void scheduleNotification(NotificationData data, NotificationService service) throws SchedulerException {
+        JobDataMap jobDataMap = new JobDataMap();
+        jobDataMap.put("notificationData", data);
+        jobDataMap.put("notificationService", service);
+        JobDetail job = JobBuilder.newJob(NotificationJob.class)
+                .withIdentity("notificationJob", "notificationGroup") // TODO: aqui o grupo podia ser o id do utilizador/produto
+                .usingJobData(jobDataMap)
+                .build();
+
+        Date triggerDate = Date.from(data.getDateToSend().atZone(ZoneId.systemDefault()).toInstant());
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .withIdentity("notificationTrigger", "notificationGroup") // TODO: ver isto tb
+                .startAt(triggerDate)
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule().withMisfireHandlingInstructionFireNow())
+                .build();
+
+        Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+        scheduler.start();
+        scheduler.scheduleJob(job, trigger);
+    }
+
+    public static class NotificationJob implements Job {
+
+        public NotificationJob() {
+        }
+
+        public void execute(JobExecutionContext context) {
+            JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
+            NotificationData data = (NotificationData) jobDataMap.get("notificationData");
+            NotificationService service = (NotificationService) jobDataMap.get("notificationService");
+
+            service.notify(data);
+        }
+    }
+}
--- src/main/java/pt/up/fe/pe25/task/notification/plugins/WhatsAppPlugin.java
+++ src/main/java/pt/up/fe/pe25/task/notification/plugins/WhatsAppPlugin.java
@@ -16,45 +16,45 @@ public class WhatsAppPlugin extends PluginDecorator{
         if (notificationService != null)
             super.notify(notificationData);
         System.out.println("Sending WhatsApp notification");
-        try {
-            String product_id = "5ea95684-b279-4cc9-aba9-872a5bdf4a52";
-            String phone_id = "27088";
-            String maytapiKey = "e1a4c1bf-90ca-43c5-8563-86ea773455dd";
-
-            URL url = new URL("https://api.maytapi.com/api/" + product_id + "/" + phone_id + "/sendMessage");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setRequestProperty("x-maytapi-key", maytapiKey);
-            conn.setDoOutput(true);
-
-            String to_number = notificationData.getReceiverPhone();
-            String message = notificationData.getMessage();
-
-            String requestBody = "{\"to_number\": \"" + to_number + "\"," +
-                    " \"type\": \"text\", " +
-                    "\"message\": \"" + message + "\"}";
-
-            OutputStream os = conn.getOutputStream();
-            os.write(requestBody.getBytes());
-            os.flush();
-            os.close();
-
-            int responseCode = conn.getResponseCode();
-            System.out.println("Response code: " + responseCode);
-
-            InputStream is = conn.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String line;
-            StringBuilder response = new StringBuilder();
-            while ((line = br.readLine()) != null) {
-                response.append(line);
-            }
-            br.close();
-            System.out.println("Response body: " + response);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // try {
+        //     String product_id = "5ea95684-b279-4cc9-aba9-872a5bdf4a52";
+        //     String phone_id = "27088";
+        //     String maytapiKey = "e1a4c1bf-90ca-43c5-8563-86ea773455dd";
+//
+        //     URL url = new URL("https://api.maytapi.com/api/" + product_id + "/" + phone_id + "/sendMessage");
+        //     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        //     conn.setRequestMethod("POST");
+        //     conn.setRequestProperty("Content-Type", "application/json");
+        //     conn.setRequestProperty("x-maytapi-key", maytapiKey);
+        //     conn.setDoOutput(true);
+//
+        //     String to_number = notificationData.getReceiverPhone();
+        //     String message = notificationData.getMessage();
+//
+        //     String requestBody = "{\"to_number\": \"" + to_number + "\"," +
+        //             " \"type\": \"text\", " +
+        //             "\"message\": \"" + message + "\"}";
+//
+        //     OutputStream os = conn.getOutputStream();
+        //     os.write(requestBody.getBytes());
+        //     os.flush();
+        //     os.close();
+//
+        //     int responseCode = conn.getResponseCode();
+        //     System.out.println("Response code: " + responseCode);
+//
+        //     InputStream is = conn.getInputStream();
+        //     BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        //     String line;
+        //     StringBuilder response = new StringBuilder();
+        //     while ((line = br.readLine()) != null) {
+        //         response.append(line);
+        //     }
+        //     br.close();
+        //     System.out.println("Response body: " + response);
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
         return false;
     }
 }
