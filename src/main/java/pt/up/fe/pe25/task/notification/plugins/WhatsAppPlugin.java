ee48b078d0301b17080921716c0437e9edef765e
th0rz05 <73318820+th0rz05@users.noreply.github.com>
2023-03-04 19:40:25 +0000

    add various functions to send multiple type of messages through the whatsapp api

--- src/main/java/pt/up/fe/pe25/task/notification/plugins/WhatsAppPlugin.java
+++ src/main/java/pt/up/fe/pe25/task/notification/plugins/WhatsAppPlugin.java
@@ -11,9 +11,9 @@ import org.json.JSONObject;
 
 public class WhatsAppPlugin extends PluginDecorator{
 
-    private static final String PRODUCT_ID = "XXXXXXX";
-    private static final String PHONE_ID = "XXXXX";
-    private static final String MAYTAPI_KEY = "XXXXXXX";
+    private static final String PRODUCT_ID = "XXXXX";
+    private static final String PHONE_ID = "XXXX";
+    private static final String MAYTAPI_KEY = "XXXXX";
 
     public WhatsAppPlugin(NotificationService notificationService) {
         super(notificationService);
@@ -25,9 +25,11 @@ public class WhatsAppPlugin extends PluginDecorator{
             super.notify(notificationData);
 
         try {
-            String group_id = createGroup(notificationData.getTicketId(),notificationData.getReceiverPhone());
-            System.out.println("Group id:" + group_id);
-            sendMessage(notificationData.getMessage(),group_id);
+            JSONObject jsonResponse = sendLinkMessage("https://latitude.to/articles-by-country/pt/portugal/8013/estadio-do-dragao",
+                    "Estádio do Dragão","XXXXXXXXXXX");
+            if(jsonResponse.getBoolean("success"))
+                System.out.println("successful operation");
+            else System.out.println(jsonResponse.getString("message"));
         } catch (IOException | JSONException e) {
             e.printStackTrace();
         }
@@ -35,22 +37,123 @@ public class WhatsAppPlugin extends PluginDecorator{
         return false;
     }
 
-    public static String createGroup(String ticketId, List<String> numbers) throws IOException, JSONException {
-        String groupName = "TICKET #" + ticketId;
+    /**
+     * Creates a new WhatsApp group with the given name and phone numbers.
+     *
+     * @param groupName the name of the group
+     * @param phoneNumbers a list of phone numbers to add to the group
+     * @return a JSONObject representing the request's response
+     * @throws IOException if there is an error sending the request
+     * @throws JSONException if there is an error parsing the JSON response
+     */
+    public static JSONObject createGroup(String groupName, List<String> phoneNumbers) throws IOException, JSONException {
         String requestBody = "{\"name\": \"" + groupName + "\", " +
-                "\"numbers\": " + new JSONArray(numbers) + "}";
-        JSONObject jsonResponse = sendPostRequest("https://api.maytapi.com/api/" + PRODUCT_ID + "/" + PHONE_ID + "/createGroup", requestBody);
-        return jsonResponse.getJSONObject("data").getString("id");
+                "\"numbers\": " + new JSONArray(phoneNumbers) + "}";
+        return sendPostRequest("https://api.maytapi.com/api/" + PRODUCT_ID + "/" + PHONE_ID + "/createGroup", requestBody);
     }
 
-    public static Boolean sendMessage(String message, String group_id) throws IOException, JSONException {
-        String requestBody = "{\"to_number\": \"" + group_id + "\"," +
-                " \"type\": \"text\", " +
+    /**
+     * Adds or removes a phone number from a WhatsApp group.
+     *
+     * @param groupId the ID of the group
+     * @param phoneNumber the phone number to add or remove
+     * @param isAddOperation true to add the phone number, false to remove it
+     * @return a JSONObject representing the request's response
+     * @throws IOException if there is an error sending the request
+     * @throws JSONException if there is an error parsing the JSON response
+     */
+    public static JSONObject updateGroup(String groupId, String phoneNumber, boolean isAddOperation) throws IOException, JSONException {
+        String endpoint = isAddOperation ? "add" : "remove";
+        String requestBody = "{\"conversation_id\": \"" + groupId + "\", " +
+                "\"number\": " + phoneNumber + "}";
+        return sendPostRequest("https://api.maytapi.com/api/" + PRODUCT_ID + "/" + PHONE_ID + "/group/" + endpoint, requestBody);
+    }
+
+
+    /**
+     * Used to send a text message to an individual person or group chat.
+     *
+     * @param message the text message to send
+     * @param receiver the phone number or group ID of the message recipient
+     * @return a JSONObject representing the request's response
+     * @throws IOException if there is an error sending the request
+     * @throws JSONException if there is an error parsing the JSON response
+     */
+    public static JSONObject sendTextMessage(String message, String receiver) throws IOException, JSONException {
+        String requestBody = "{\"to_number\": \"" + receiver + "\", " +
+                "\"type\": \"text\", " +
                 "\"message\": \"" + message + "\"}";
-        sendPostRequest("https://api.maytapi.com/api/" + PRODUCT_ID + "/" + PHONE_ID + "/sendMessage", requestBody);
-        return true;
+        return sendPostRequest("https://api.maytapi.com/api/" + PRODUCT_ID + "/" + PHONE_ID + "/sendMessage", requestBody);
     }
 
+    /**
+     * Used to send a media message to an individual person or group chat.
+     *
+     * @param media the Base64-encoded media content to send (e.g., an image or video) or a URL to that media
+     * @param caption the optional caption text for the media message
+     * @param receiver the phone number or group ID of the message recipient
+     * @return a JSONObject representing the request's response
+     * @throws IOException if there is an error sending the request
+     * @throws JSONException if there is an error parsing the JSON response
+     */
+    public static JSONObject sendMediaMessage(String media, String caption, String receiver) throws IOException, JSONException {
+        String requestBody = "{\"to_number\": \"" + receiver + "\", " +
+                "\"type\": \"media\", " +
+                "\"message\": \"" + media + "\", " +
+                "\"text\": \"" + caption + "\"}";
+        return sendPostRequest("https://api.maytapi.com/api/" + PRODUCT_ID + "/" + PHONE_ID + "/sendMessage", requestBody);
+    }
+
+    /**
+     * Used to send a location message to an individual person or group chat.
+     *
+     * @param latitude the latitude of the location in decimal degrees (DD)
+     * @param longitude the longitude of the location in decimal degrees (DD)
+     * @param locationText the name or description of the location
+     * @param receiver the phone number or group ID of the message recipient
+     * @return a JSONObject representing the request's response
+     * @throws IOException if there is an error sending the request
+     * @throws JSONException if there is an error parsing the JSON response
+     */
+    public static JSONObject sendLocationMessage(String latitude, String longitude, String locationText, String receiver) throws IOException, JSONException {
+        String requestBody = "{\"to_number\": \"" + receiver + "\", " +
+                "\"type\": \"location\", " +
+                "\"text\": \"" + locationText + "\", " +
+                "\"latitude\": \"" + latitude + "\", " +
+                "\"longitude\": \"" + longitude + "\"}";
+        return sendPostRequest("https://api.maytapi.com/api/" + PRODUCT_ID + "/" + PHONE_ID + "/sendMessage", requestBody);
+    }
+
+    /**
+     * Used to send a link message to an individual person or group chat.
+     *
+     * @param url the link to send
+     * @param text optional text to include with the link
+     * @param receiver the phone number or group ID of the message recipient
+     * @return a JSONObject representing the request's response
+     * @throws IOException if there is an error sending the request
+     * @throws JSONException if there is an error parsing the JSON response
+     */
+    public static JSONObject sendLinkMessage(String url, String text, String receiver) throws IOException, JSONException {
+        String requestBody = "{\"to_number\": \"" + receiver + "\"," +
+                " \"type\": \"link\", " +
+                "\"message\": \"" + url + "\", " +
+                "\"text\": \"" + text + "\"}";
+        return sendPostRequest("https://api.maytapi.com/api/" + PRODUCT_ID + "/" + PHONE_ID + "/sendMessage", requestBody);
+    }
+
+
+
+
+    /**
+     * Sends an HTTP POST request to the specified URL with the given request body.
+     *
+     * @param urlString the URL to send the request to
+     * @param requestBody the request body to send
+     * @return a JSONObject representing the response from the server
+     * @throws IOException if there is an error sending or receiving the request
+     * @throws JSONException if there is an error parsing the JSON response
+     */
     public static JSONObject sendPostRequest(String urlString, String requestBody) throws IOException, JSONException {
         URL url = new URL(urlString);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
