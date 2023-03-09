677a61593938fffa4163f8e5ffa32b93dce291c8
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2023-03-09 21:58:02 +0000

    Adding multiple schedulers

--- src/main/java/pt/up/fe/pe25/task/notification/NotificationScheduler.java
+++ src/main/java/pt/up/fe/pe25/task/notification/NotificationScheduler.java
@@ -15,13 +15,13 @@ public class NotificationScheduler {
         jobDataMap.put("notificationData", data);
         jobDataMap.put("notificationService", service);
         JobDetail job = JobBuilder.newJob(NotificationJob.class)
-                .withIdentity("notificationJob", "notificationGroup") // TODO: aqui o grupo podia ser o id do utilizador/produto
+                .withIdentity("notificationJob" + data.hashCode(), "notificationGroup") // TODO: aqui o grupo podia ser o id do utilizador/produto
                 .usingJobData(jobDataMap)
                 .build();
 
         Date triggerDate = Date.from(data.getDateToSend().atZone(ZoneId.systemDefault()).toInstant());
         Trigger trigger = TriggerBuilder.newTrigger()
-                .withIdentity("notificationTrigger", "notificationGroup") // TODO: ver isto tb
+                .withIdentity("notificationTrigger" + data.hashCode(), "notificationGroup") // TODO: ver isto tb
                 .startAt(triggerDate)
                 .withSchedule(SimpleScheduleBuilder.simpleSchedule().withMisfireHandlingInstructionFireNow())
                 .build();
