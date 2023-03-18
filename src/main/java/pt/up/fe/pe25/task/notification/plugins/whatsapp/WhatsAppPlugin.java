33cbf153c90030f05964e701270e010f6b19ae4f
th0rz05 <73318820+th0rz05@users.noreply.github.com>
2023-03-18 02:19:55 +0000

    refactoring code

--- src/main/java/pt/up/fe/pe25/task/notification/plugins/whatsapp/WhatsAppPlugin.java
+++ src/main/java/pt/up/fe/pe25/task/notification/plugins/whatsapp/WhatsAppPlugin.java
@@ -1,19 +1,15 @@
 package pt.up.fe.pe25.task.notification.plugins.whatsapp;
 
-import pt.up.fe.pe25.task.notification.plugins.whatsapp.WhatsAppProperties;
 import org.json.JSONArray;
 import pt.up.fe.pe25.task.notification.NotificationData;
 import pt.up.fe.pe25.task.notification.NotificationService;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.json.JSONObject;
-
-
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-
 import pt.up.fe.pe25.task.notification.plugins.PluginDecorator;
 
 
@@ -71,23 +67,16 @@ public class WhatsAppPlugin extends PluginDecorator {
      *
      * @param groupName the name of the group
      * @param phoneNumbers a list of phone numbers to add to the group
-     * @return the ID of the new group if the request is successful, false otherwise
+     * @return the ID of the new group if the request is successful ; throws an exception otherwise
      */
-    public Object createGroup(String groupName, List<String> phoneNumbers) {
+    public String createGroup(String groupName, List<String> phoneNumbers) {
         String url = "https://api.maytapi.com/api/" + PRODUCT_ID + "/" + PHONE_ID + "/createGroup";
         String requestBody = "{\"name\": \"" + groupName + "\", " +
                 "\"numbers\": " + new JSONArray(phoneNumbers) + "}";
         JSONObject response = sendRequest(url, requestBody, httpMethod, headers);
-        if (response == null) {
-            throw new IllegalArgumentException("Failed to create group. Response is null.");
-        }
-        boolean success = response.getBoolean("success");
-        if (success) {
-            return response.getJSONObject("data").getString("id");
-        } else {
-            String message = response.getString("message");
-            throw new IllegalArgumentException("Failed to create group. Reason: " + message);
-        }
+        AtomicReference<String> groupIdRef = new AtomicReference<>("");
+        processResponse(response, groupIdRef);
+        return groupIdRef.get();
     }
 
     /**
@@ -96,33 +85,23 @@ public class WhatsAppPlugin extends PluginDecorator {
      * @param groupId the ID of the group
      * @param phoneNumber the phone number to add or remove
      * @param isAddOperation true to add the phone number, false to remove it
-     * @return true if the message was sent successfully; false otherwise
+     * @return true if the message was sent successfully; throws an exception otherwise
      */
-    public  boolean updateGroup(String groupId, String phoneNumber, boolean isAddOperation){
+    public boolean updateGroup(String groupId, String phoneNumber, boolean isAddOperation){
         String endpoint = isAddOperation ? "add" : "remove";
         String url = "https://api.maytapi.com/api/" + PRODUCT_ID + "/" + PHONE_ID + "/group/" + endpoint;
         String requestBody = "{\"conversation_id\": \"" + groupId + "\", " +
                 "\"number\": " + phoneNumber + "}";
         JSONObject response = sendRequest(url, requestBody, httpMethod, headers);
-        if (response == null) {
-            throw new IllegalArgumentException("Failed to update group. Response is null.");
-        }
-        boolean success = response.getBoolean("success");
-        if (success) {
-            return true;
-        } else {
-            String message = response.getString("message");
-            throw new IllegalArgumentException("Failed to update group. Reason: " + message);
-        }
+        return processResponse(response);
     }
-
 
     /**
      * Used to send a text message to an individual person or group chat.
      *
      * @param text the text message to send
      * @param receiver the phone number or group ID of the message recipient
-     * @return true if the message was sent successfully; false otherwise
+     * @return true if the message was sent successfully; throws an exception otherwise
      */
     public boolean sendTextMessage(String text, String receiver){
         String url = "https://api.maytapi.com/api/" + PRODUCT_ID + "/" + PHONE_ID + "/sendMessage";
@@ -130,16 +109,7 @@ public class WhatsAppPlugin extends PluginDecorator {
                 "\"type\": \"text\", " +
                 "\"message\": \"" + text + "\"}";
         JSONObject response = sendRequest(url, requestBody, httpMethod, headers);
-        if (response == null) {
-            throw new IllegalArgumentException("Failed to send text message. Response is null.");
-        }
-        boolean success = response.getBoolean("success");
-        if (success) {
-            return true;
-        } else {
-            String message = response.getString("message");
-            throw new IllegalArgumentException("Failed to send text message. Reason: " + message);
-        }
+        return processResponse(response);
     }
 
     /**
@@ -148,7 +118,7 @@ public class WhatsAppPlugin extends PluginDecorator {
      * @param media the Base64-encoded media content to send (e.g., an image or video) or a URL to that media
      * @param caption the optional caption text for the media message
      * @param receiver the phone number or group ID of the message recipient
-     * @return true if the message was sent successfully; false otherwise
+     * @return true if the message was sent successfully; throws an exception otherwise
      */
     public boolean sendMediaMessage(String media, String caption, String receiver){
         String url = "https://api.maytapi.com/api/" + PRODUCT_ID + "/" + PHONE_ID + "/sendMessage";
@@ -157,16 +127,7 @@ public class WhatsAppPlugin extends PluginDecorator {
                 "\"message\": \"" + media + "\", " +
                 "\"text\": \"" + caption + "\"}";
         JSONObject response = sendRequest(url, requestBody, httpMethod, headers);
-        if (response == null) {
-            throw new IllegalArgumentException("Failed to send media message. Response is null.");
-        }
-        boolean success = response.getBoolean("success");
-        if (success) {
-            return true;
-        } else {
-            String message = response.getString("message");
-            throw new IllegalArgumentException("Failed to send media message. Reason: " + message);
-        }
+        return processResponse(response);
     }
 
     /**
@@ -176,7 +137,7 @@ public class WhatsAppPlugin extends PluginDecorator {
      * @param longitude the longitude of the location in decimal degrees (DD)
      * @param locationText the name or description of the location
      * @param receiver the phone number or group ID of the message recipient
-     * @return true if the message was sent successfully; false otherwise
+     * @return true if the message was sent successfully; throws an exception otherwise
      */
     public boolean sendLocationMessage(String latitude, String longitude, String locationText, String receiver){
         String url = "https://api.maytapi.com/api/" + PRODUCT_ID + "/" + PHONE_ID + "/sendMessage";
@@ -186,16 +147,7 @@ public class WhatsAppPlugin extends PluginDecorator {
                 "\"latitude\": \"" + latitude + "\", " +
                 "\"longitude\": \"" + longitude + "\"}";
         JSONObject response = sendRequest(url, requestBody, httpMethod, headers);
-        if (response == null) {
-            throw new IllegalArgumentException("Failed to send location message. Response is null.");
-        }
-        boolean success = response.getBoolean("success");
-        if (success) {
-            return true;
-        } else {
-            String message = response.getString("message");
-            throw new IllegalArgumentException("Failed to send location message. Reason: " + message);
-        }
+        return processResponse(response);
     }
 
 
@@ -205,7 +157,7 @@ public class WhatsAppPlugin extends PluginDecorator {
      * @param link     the link to send
      * @param text     optional text to include with the link
      * @param receiver the phone number or group ID of the message recipient
-     * @return true if the message was sent successfully; false otherwise
+     * @return true if the message was sent successfully; throws an exception otherwise
      */
     public boolean sendLinkMessage(String link, String text, String receiver) {
         String url = "https://api.maytapi.com/api/" + PRODUCT_ID + "/" + PHONE_ID + "/sendMessage";
@@ -214,16 +166,41 @@ public class WhatsAppPlugin extends PluginDecorator {
                 "\"message\": \"" + link + "\", " +
                 "\"text\": \"" + text + "\"}";
         JSONObject response = sendRequest(url, requestBody, httpMethod, headers);
+        return processResponse(response);
+    }
+
+    /**
+     * Used to process the response from the API.
+     * @param response the response from the API
+     * @return true if the request was successful; throws an exception otherwise
+     */
+    private boolean processResponse(JSONObject response) {
+        return processResponse(response, null);
+    }
+
+    /**
+     * Used to process the response from the API.
+     * @param response the response from the API
+     * @param groupId the ID of the group if the request for creating it is successful
+     * @return true if the request was successful; throws an exception otherwise
+     */
+    private boolean processResponse(JSONObject response, AtomicReference<String> groupId) {
         if (response == null) {
-            throw new IllegalArgumentException("Failed to send link message. Response is null.");
+            throw new IllegalArgumentException("Failure - Reason : Response is null.");
         }
         boolean success = response.getBoolean("success");
         if (success) {
+            if (groupId != null) {
+                groupId.set(response.getJSONObject("data").getString("id"));
+            }
             return true;
         } else {
             String message = response.getString("message");
-            throw new IllegalArgumentException("Failed to send link message. Reason: " + message);
+            throw new IllegalArgumentException("Failure - Reason: " + message);
         }
     }
+
+
+
 
 }
